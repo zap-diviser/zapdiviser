f4c04a7aba2cf8d1c30912c519f4d4e039950f82
Vitor Daniel <vitor036daniel@gmail.com>
2024-03-11 20:18:27 -0300

    fix

--- backend/src/modules/product/product.module.ts
+++ backend/src/modules/product/product.module.ts
@@ -28,7 +28,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         endPoint: configService.get<string>('MINIO_ENDPOINT')!,
         port: parseInt(configService.get<string>('MINIO_PORT')!),
-        useSSL: false,
+        useSSL: true,
         accessKey: configService.get<string>('MINIO_ACCESS_KEY')!,
         secretKey: configService.get<string>('MINIO_SECRET_KEY')!,
         bucket: 'zapdiviser',
