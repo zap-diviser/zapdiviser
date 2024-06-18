2f0537069323879f0053a9098da09cef993d79b7
Vitor Daniel <vitor036daniel@gmail.com>
2024-06-18 03:54:36 -0300

    fix

--- frontend/src/pages/panel/whatsapps/QRWhatsapp.tsx
+++ frontend/src/pages/panel/whatsapps/QRWhatsapp.tsx
@@ -68,9 +68,6 @@ const QRWhatsapp = ({ isOpen, onClose, data }: Props) => {
       }
 
       // @ts-ignore
-      console.log(import.meta.env.VITE_SOKETI_APP_KEY!);
-
-      // @ts-ignore
       const client = new Pusher(import.meta.env.VITE_SOKETI_APP_KEY!, options);
       const channel = client.subscribe(`whatsapp-${data.id}`);
 
